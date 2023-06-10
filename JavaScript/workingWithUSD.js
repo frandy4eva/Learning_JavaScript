f1ef8eb432275540b97b0835d6870abd7c47d82e
frandy4eva <129144410+frandy4eva@users.noreply.github.com>
2023-06-10 07:21:40 -0500

    Update workingWithUSD.js
    Add commits

--- JavaScript/workingWithUSD.js
+++ JavaScript/workingWithUSD.js
@@ -2,10 +2,10 @@
 // 2 basketball at $20.95 each with $4.99 shipping
 // and 2 t-shirts at $7.99 each with $4.99 shipping
 
-let itemsTotal = `Items (4):  $${((2095 + 799) * 2) / 100}`;
+let itemsTotal = `Items (4):  $${((2095 + 799) * 2) / 100}`; // Convert from Dollar to cents by omitting the period 2095, and back to Dollar by dividing by 100
 let shippingHandlingTotal = `Shipping & handling:  $${(499 * 2) / 100}`;
 let preTaxTotal = `Total before tax: $${(5788 + 998) / 100}`;
-let estimatedTax = `Estimated tax (10%): $${Math.round(6786 * 0.1) / 100}`;
+let estimatedTax = `Estimated tax (10%): $${Math.round(6786 * 0.1) / 100}`; // Use Math.round() when calculating tax for accuracy.
 let orderTotal = `Order total: $${(679 + 6786) / 100}`;
 
 console.log(`
